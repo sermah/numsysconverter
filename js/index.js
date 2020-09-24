b43f1ae0cdf477bb98bc4b8d3041ead12a0039f4
sermah <2004mah@gmail.com>
2020-09-24 15:53:32 +0300

    Structure + russian

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 var alphabet = [];
 
+//ASCII код 0 - 48, A - 65
+
 $(function(){
     for(let i = 0; i < 10; i++){
         alphabet.push(String.fromCharCode(48 + i));
@@ -17,17 +19,27 @@ function convert(){
         curNum = 0;
 
     for(let i = fromNum.length - 1; i >= 0; i--){
-        curNum += alphabet.indexOf(fromNum[i]) * Math.pow(fromSys,fromNum.length - 1 - i);
+        let num = alphabet.indexOf(fromNum[i]);
+        if(num >= fromSys){
+            convertError("Число содержит неподходящие символы, используйте 0-9 и A-Z, если система счисления имеет такие цифры/буквы.");
+            // Number contains invalid characters. Please use 0-9 and A-Z if the numerical system has such letters/digits.
+            return;
+        }
+        curNum += num * Math.pow(fromSys,fromNum.length - 1 - i);
     }
 
-    console.log(fromNum, curNum);
+    //console.log(fromNum, curNum);
 
-    for(let i = 0; curNum != 0; i++){
+    if(toSys == 10) toNum = curNum.toString();
+    else for(let i = 0; curNum != 0; i++){
         let digit = parseInt(curNum % toSys);
         curNum = parseInt(curNum / toSys);
-
         toNum = alphabet[digit] +  ((i % 3 == 0 && i > 0) ? " " : "") + toNum;
     }
 
     $("#outputNum").html(toNum);
 };
+
+function convertError(reason){
+    $("#outputNum").html("Ошибка: " + reason);
+}
